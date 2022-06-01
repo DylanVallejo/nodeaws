63242363c46b86f157cb4b50976187bf2722a9a0
dylan <dylan1811vallejo@gmail.com>
2022-06-01 13:14:29 -0500

    puerto nuevo

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 
 
-app.get('/', (req, res) =>('<h1>Express con html /h1>'));
-
+app.get('/', (req, res) => {
+    res.send ('Hello World express');
+}
+)
 
 app.listen(8080);
 console.log("server is running on port 8080");
